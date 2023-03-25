66cb8bde9d1fb80e9de129fa97fdfa1753278200
dineshpawar123 <46311822+dineshpawar123@users.noreply.github.com>
2023-03-25 19:54:23 +0530

    Dev (#4)
    * change app.js with new msg
    
    * change app.js
    
    * my-name component added (#3)

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-  Hello Dinesh !!!!! Have a nice weekend
+          Hello Dinesh !!!!! Have a nice weekend
       </header>
     </div>
   );
