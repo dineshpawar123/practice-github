5015229850f8dfe9169a22eaaf75e51783939cbb
dineshpawar123 <46311822+dineshpawar123@users.noreply.github.com>
2023-04-06 07:24:58 +0530

    Feature/merge (#9)
    * msg 1 added in feature/merge
    
    * msg 2 added in feature/merge
    
    * merge child msg 1 added
    
    * merge child msg 2 added
    
    * commit msg 3 added in feature/merge
    
    * commit msg 4 added in feature/merge

--- src/components/mergeFile.js
+++ src/components/mergeFile.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 function MergeFile() {
   return (
-    <div>MergeFile</div>
+    <div>
+        <div>MergeFile</div>
+        <div>commit msg 1 added in feature/merge</div>
+        <div>commit msg 2 added in feature/merge</div>
+        <div>merge child msg 1 added</div>
+        <div>merge child msg 2 added</div>
+
+
+        <div>commit msg 3 added in feature/merge</div>
+
+        <div>commit msg 4 added in feature/merge</div>
+    </div>
   )
 }
 
